91a63c8e62c2fcecef74d399fe39adcfc3938672
Maamar Khemissi <maamar.khemissi@meingutscheincode.com>
2019-02-05 16:53:42 +0100

    ajouter mots

---
+++ src/Entity/Mot.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mimosa
+ * Date: 05.02.19
+ * Time: 16:29
+ */
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\MotRepository")
+ */
+class Mot
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $updatedAt;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $inLatin;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $inTamazight;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $inArabic;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * Mot constructor.
+     * @throws \Exception
+     */
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getInLatin(): ?string
+    {
+        return $this->inLatin;
+    }
+
+    public function setInLatin(string $inLatin): self
+    {
+        $this->inLatin = $inLatin;
+
+        return $this;
+    }
+
+    public function getInTamazight(): ?string
+    {
+        return $this->inTamazight;
+    }
+
+    public function setInTamazight(?string $inTamazight): self
+    {
+        $this->inTamazight = $inTamazight;
+
+        return $this;
+    }
+
+    public function getInArabic(): ?string
+    {
+        return $this->inArabic;
+    }
+
+    public function setInArabic(?string $inArabic): self
+    {
+        $this->inArabic = $inArabic;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
